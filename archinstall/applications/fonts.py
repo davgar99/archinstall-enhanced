9ea6f089e434002dd7740ed010bf25c0b6dbb61a
David <davidgarcia117@gmail.com>
2026-09-13 20:49:29 -0400

    Add system-wide font rendering baseline

--- archinstall/applications/fonts.py
+++ archinstall/applications/fonts.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from archinstall.lib.log import debug
@@ -5,6 +6,38 @@ from archinstall.lib.models.application import FontsConfiguration
 
 if TYPE_CHECKING:
 	from archinstall.lib.installer import Installer
+
+
+BITMAP_PRESET_NAME = '70-no-bitmaps-except-emoji.conf'
+BITMAP_PRESET_TARGET = Path('/usr/share/fontconfig/conf.avail') / BITMAP_PRESET_NAME
+RENDERING_PRESET_NAME = '90-archinstall-enhanced-rendering.conf'
+RENDERING_PRESET = '''<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+<fontconfig>
+  <match target="font">
+    <edit name="antialias" mode="assign"><bool>true</bool></edit>
+    <edit name="hinting" mode="assign"><bool>true</bool></edit>
+    <edit name="hintstyle" mode="assign"><const>hintslight</const></edit>
+    <edit name="lcdfilter" mode="assign"><const>lcddefault</const></edit>
+  </match>
+</fontconfig>
+'''
+
+
+def configure_font_rendering(target: Path) -> None:
+	"""Install conservative system-wide Fontconfig defaults for desktop sessions."""
+	conf_dir = target / 'etc/fonts/conf.d'
+	conf_dir.mkdir(parents=True, exist_ok=True)
+
+	bitmap_link = conf_dir / BITMAP_PRESET_NAME
+	if bitmap_link.is_symlink():
+		if bitmap_link.readlink() != BITMAP_PRESET_TARGET:
+			bitmap_link.unlink()
+			bitmap_link.symlink_to(BITMAP_PRESET_TARGET)
+	elif not bitmap_link.exists():
+		bitmap_link.symlink_to(BITMAP_PRESET_TARGET)
+
+	(conf_dir / RENDERING_PRESET_NAME).write_text(RENDERING_PRESET, encoding='utf-8')
 
 
 class FontsApp:
